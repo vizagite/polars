165d46ae8145e97235828966ee747020359c5d75
Simon Lin <simonlin.rqmmw@slmail.me>
2026-06-25 19:41:51 +1000

    c

--- crates/polars-core/src/series/implementations/duration.rs
+++ crates/polars-core/src/series/implementations/duration.rs
@@ -530,11 +530,13 @@ impl SeriesTrait for SeriesWrap<DurationChunked> {
         let v = sc.value().as_duration(self.0.time_unit());
         Ok(Scalar::new(self.dtype().clone(), v))
     }
+
     fn min_reduce(&self) -> PolarsResult<Scalar> {
         let sc = self.0.physical().min_reduce();
         let v = sc.value().as_duration(self.0.time_unit());
         Ok(Scalar::new(self.dtype().clone(), v))
     }
+
     fn std_reduce(&self, ddof: u8) -> PolarsResult<Scalar> {
         let sc = self.0.physical().std_reduce(ddof);
         let to = self.dtype().to_physical();
